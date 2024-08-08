4f45a6b5dc2b141a172cfde0193856ebd677627d
esakkiraja-k <EsakkiRaja@KANINI-LTP-1122>
2024-08-08 18:52:43 +0530

    http services

--- apm-begin/src/app/products/product-detail/product-detail.component.ts
+++ apm-begin/src/app/products/product-detail/product-detail.component.ts
@@ -1,15 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 
 import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
 import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { Subscription,pipe,tap } from 'rxjs';
 
 @Component({
-    selector: 'pm-product-detail',
-    templateUrl: './product-detail.component.html',
-    standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe]
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, CurrencyPipe]
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnChanges, OnDestroy {
+
+  /**
+   *
+   */
+  constructor(private productSvc: ProductService) {
+
+
+  }
+  sub!: Subscription;
   // Just enough here for the template to compile
   @Input() productId: number = 0;
   errorMessage = '';
@@ -22,4 +33,23 @@ export class ProductDetailComponent {
 
   addToCart(product: Product) {
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    const id = changes['productId'].currentValue;
+    if (id) {
+      this.sub = this.productSvc.getProduct(id)
+        .pipe(tap(data => console.log(data)))
+        .subscribe(
+          p => this.product = p
+        );
+    }
+  }
+
+  ngOnDestroy(): void {
+    if(this.sub){
+      this.sub.unsubscribe();
+    }
+  }
+
 }
--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,10 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  // Just enough here for the code to compile
+  
   private productsUrl = 'api/products';
 
+constructor(private http: HttpClient) {  
+  
+}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl)
+      .pipe(
+        tap(() => console.log('http pipeline'))
+      );
+  }
+
+  getProduct(id:Number){
+    const productUrlWithId = this.productsUrl + '/' + id;
+    return this.http.get<Product>(productUrlWithId)
+    .pipe(
+      tap(() => console.log('http get by id Service pipeline'))
+    );
+  }
 }
